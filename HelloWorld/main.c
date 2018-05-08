98251b71f33144921b370c19f3a27c6df9e6a1f8
Gavin Nickerson <oya.gavin@gmail.com>
2018-05-07 21:12:02 -0400

    Initial Commit

---
+++ HelloWorld/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HelloWorld
+//
+//  Created by Gavin Nickerson  on 2018-05-07.
+//  Copyright © 2018 Gavin Nickerson . All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
